15857daf9b28291093fbc926329271b0131d904a
zyqunix <117040076+zyqunix@users.noreply.github.com>
2024-01-15 19:58:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const audio = new Audio('music/sunset.mp3')
+audio.autoplay = true;
+audio.loop = true;
+audio.volume = 0.1;
+
+const textarea = document.getElementById('passwordInput');
+
+textarea.addEventListener('keydown', (e) => {
+  if (e.key === 13) {
+    e.preventDefault();
+  }
+});
+
+function checkPassword() {
+  const password = document.getElementById('passwordInput').value;
+  const passwords = ['gockelhahn', 'yaraxtendo', 'bulachi'];
+
+  if (passwords.includes(password)) {
+    window.location.href = 'success.html';
+  } else {
+    document.getElementById('message').textContent = 'try again bro';
+  }
+}
